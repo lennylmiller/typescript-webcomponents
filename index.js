d3c3e4fb1ad71f7094c1bb6ebfb405bdbcd61e53
Lenny Miller <lenny.l.miller@gmail.com>
2024-06-16 15:01:48 -0700

    added a new idea

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
 class TimeFormat extends HTMLElement {
-    connectedCallback() {
-      let date = new Date(this.getAttribute('datetime') || Date.now());
-      this.innerHTML = new Intl.DateTimeFormat('default', {
-        year: this.getAttribute('year') || undefined,
-        month: this.getAttribute('month') || undefined,
-        day: this.getAttribute('day') || undefined,
-        hour: this.getAttribute('hour') || undefined,
-        minute: this.getAttribute('minute') || undefined,
-        second: this.getAttribute('second') || undefined,
-        timeZoneName: this.getAttribute('time-zone-name') || undefined,
-      }).format(date);
-    }
+  static get observedAttributes() {
+    return ['datetime','year','month','day','hour','minute','second','time-zone-name'];
+  }
+  connectedCallback() {
+    const datetime = this.getAttribute("datetime");
+    let date = new Date(datetime || Date.now());
+    this.innerHTML = new Intl.DateTimeFormat("default", {
+      year: this.getAttribute("year") || undefined,
+      month: this.getAttribute("month") || undefined,
+      day: this.getAttribute("day") || undefined,
+      hour: this.getAttribute("hour") || undefined,
+      minute: this.getAttribute("minute") || undefined,
+      second: this.getAttribute("second") || undefined,
+      timeZoneName: this.getAttribute("time-zone-name") || undefined,
+    }).format(date);
+  }
 }
-customElements.define('time-format', TimeFormat);
